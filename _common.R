261b9fef803176f36ecb18f9230630b54b5a071e
Jaehyun Song <tintstyle@gmail.com>
2024-03-25 14:41:54 +0900

    no message

--- _common.R
+++ _common.R
@@ -1,6 +1,1 @@
 pacman::p_load(tidyverse, knitr, kableExtra, gt, gtExtras)
-knitr::opts_chunk$set(dev         = "ragg_png",
-                      fig.align   = "center",
-                      dpi         = 300,
-                      fig.pos     = "H",
-                      attr.source = ".numberLines")
